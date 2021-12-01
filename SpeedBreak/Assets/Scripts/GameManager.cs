ceb3f9186944f803324f2e8144244d12cd8d72d9
Garrett <73368079+GareBear3602@users.noreply.github.com>
2021-12-01 15:59:18 -0500

    Added Laps and Trigger

--- SpeedBreak/Assets/Scripts/GameManager.cs
+++ SpeedBreak/Assets/Scripts/GameManager.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //Lap Stuff
     public TextMeshProUGUI laptext;
     public float lapcount;
+    private float lap;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateLap(1);
     }
 
     // Update is called once per frame
@@ -21,8 +23,9 @@ public class GameManager : MonoBehaviour
 
     }
 
-    void UpdateLap()
+    public void UpdateLap(int lapAdd)
     {
-
+        lap += lapAdd;
+        laptext.text = "Lap: " + lap + "/3";
     }
 }
